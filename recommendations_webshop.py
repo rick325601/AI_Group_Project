b73d0cca250d524dfa8bcdb1b65cbfac1c401dd7
Rick van der Kleij <56115492+rick325601@users.noreply.github.com>
2023-07-07 15:17:50 +0200

    update van de docstrings

--- recommendations_webshop.py
+++ recommendations_webshop.py
@@ -3,41 +3,60 @@ from data import data
 import random
 import psycopg2
 
+# Connection parameters for PostgreSQL
 postgres_lijst = data
 
 def collaborative_filtering(connection_list, current_product_id):
-    conn = connection_postgres(connection_list[0], connection_list[1], connection_list[2],
-                               connection_list[3], connection_list[4])
+    """
+    Performs collaborative filtering to generate product recommendations based on similar product attributes.
+
+    Args:
+        connection_list (list): List of connection parameters for PostgreSQL.
+        current_product_id (str): ID of the current product.
+
+    Returns:
+        list: Recommended products based on collaborative filtering.
+    """
+    conn = connection_postgres(connection_list[0], connection_list[1], connection_list[2], connection_list[3], connection_list[4])
 
     cur = conn.cursor()
 
-    # Haal informatie op over het huidige product
+    # Fetch information about the current product
     cur.execute("SELECT * FROM products WHERE _id = (%s)", (current_product_id,))
     current_product = cur.fetchone()
 
-    # Filter de producten op basis van vergelijkbare kenmerken
+    # Filter products based on similar attributes
     cur.execute("SELECT * FROM products WHERE price BETWEEN (%s) AND (%s) AND category = (%s) AND sub_category = (%s) AND brand = (%s)",
                 (current_product[1] - 50, current_product[1] + 50, current_product[4], current_product[5], current_product[10]))
     similar_products = cur.fetchall()
 
-    # Willekeurige aanbevelingen selecteren
+    # Randomly select recommended products
     random.shuffle(similar_products)
     recommended_products = similar_products[:5]
 
     return recommended_products
 
-# Voorbeeldgebruik:
+# Example usage:
 current_product_id = '23978'
-recommendations = collaborative_filtering(data, current_product_id)
-print("collaberative filtering")
+recommendations = collaborative_filtering(postgres_lijst, current_product_id)
+print("Collaborative filtering recommendations:")
 for product in recommendations:
     print("Product ID:", product[0])
     print("Product Name:", product[2])
     print()
 
 def filter_brand_category_id(connection_list, product_id):
-    conn = connection_postgres(connection_list[0], connection_list[1], connection_list[2],
-                               connection_list[3], connection_list[4])
+    """
+    Filters products based on brand and category ID to generate recommendations.
+
+    Args:
+        connection_list (list): List of connection parameters for PostgreSQL.
+        product_id (str): ID of the product.
+
+    Returns:
+        list: Recommended products based on brand and category ID filtering.
+    """
+    conn = connection_postgres(connection_list[0], connection_list[1], connection_list[2], connection_list[3], connection_list[4])
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM products WHERE _id = (%s)", (product_id,))
@@ -64,10 +83,10 @@ def filter_brand_category_id(connection_list, product_id):
 
     return recommended_products
 
-# Voorbeeldgebruik:
+# Example usage:
 product_id = '16444'
-recommendations = filter_brand_category_id(data, product_id)
-print("Content filtering")
+recommendations = filter_brand_category_id(postgres_lijst, product_id)
+print("Content filtering recommendations:")
 for product in recommendations:
     print("Product ID:", product[0])
     print("Product Name:", product[2])
